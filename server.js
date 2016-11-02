72e69a92dbee22e7f625509608bf67d8b2be50ed
craig91 <craigdejean1@gmail.com>
2016-11-02 00:21:28 -0400

    Old node code deleted; Express code written

--- server.js
+++ server.js
@@ -1,15 +1,14 @@
-//import the http module
-var http = require('http')
+var express = require('express')
+var app = express();
+var port = 8080;
 
-//handle sending requests and returning requests
-function handleRequests(req, res){
-    // return string 
-    res.end('Hello world');
-}
-//  create the server
-var server = http.createServer(handleRequests);
-//start server and listen on port x
+//start the server
+app.listen(port, function(){
+    console.log('app started')
+});
 
-server.listen(8080, function(){
-    console.log('Listening on port 8080')
+
+// route our app
+app.get('/', function(req, res){
+    res.end('hello world again')
 });
